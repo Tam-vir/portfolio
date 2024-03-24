f9e8d174ace98632ee6d54e3916686a6aa3adeab
Tam-vir <93982408+Tam-vir@users.noreply.github.com>
2024-03-24 16:03:59 +0000

    e

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="bg-gray-800 h-full w-full">
+      <div className="text-white">
+        <h1>About Page</h1>
+      </div>
+    </main>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -13,8 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="p-6 bg-gray-900">     
       <body className={inter.className}>
-        <nav className="text-white rounded-sm">
-          <ul className="flex float-right">
+        <nav className="text-white rounded-sm bg-gray-600">
+          <ul className="flex gap-2">
             <li>
               <Link href="/"> Home </Link>
             </li>
@@ -23,6 +23,7 @@ export default function RootLayout({ children }) {
             </li>
           </ul>  
         </nav>
+        <br></br>
         <hr></hr>
         {children}
       </body>
