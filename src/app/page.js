20c7bd1ab6d74cff8288a07c62a7e38b45c4dd8f
Tam-vir <93982408+Tam-vir@users.noreply.github.com>
2024-03-26 10:25:02 +0000

    hehe

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-banner bg-cover bg-center w-full h-full p-4 flex justify-center">
-      <div className="">
+    <main className="bg-banner bg-cover bg-center w-full h-full p-4 flex align-center float-right">
+      <div className="mr-2">
       <Image src="/logo.jpg" alt="logo" width={300} height={300} />
       </div>
     </main>
